a039585b0d4b57304ce8641e555f51ea671d1806
Your Name <khimnk635@gmail.com>
2025-06-15 00:05:55 +0700

    d

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,8 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    // Add explicit engine binary targets for Vercel
-    __internal: {
-      engine: {
-        binaryTargets: ["native", "rhel-openssl-3.0.x"],
-      },
-    },
-  });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
